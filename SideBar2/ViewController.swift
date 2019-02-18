07ed12b7312f0e05029ae6d4741694d607c867e8
정기욱 <rldnr56@me.com>
2019-02-18 15:04:40 +0900

    Initial Commit

---
+++ SideBar2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideBar2
+//
+//  Created by 정기욱 on 18/02/2019.
+//  Copyright © 2019 Kiwook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
