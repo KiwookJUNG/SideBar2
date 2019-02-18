e202d2452edc7b59b018027d0840e895d44d0f31
정기욱 <rldnr56@me.com>
2019-02-18 15:16:41 +0900

    SideBarViewController 클래스는 왼쪽 옆에서 열릴 사이드 바 화면을 담당하는 뷰 컨트롤러. 테이블 뷰로 이루어지기 때문에 UITableViewController를 상속 받는다.

---
+++ SideBar2/SideBarViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SideBarViewController.swift
+//  SideBar2
+//
+//  Created by 정기욱 on 18/02/2019.
+//  Copyright © 2019 Kiwook. All rights reserved.
+//
+
+import UIKit
+
+class SideBarViewController: UITabBarController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
