ab569c62b57123dcb5a304683105a4ee10400006
정기욱 <rldnr56@me.com>
2019-02-18 15:24:36 +0900

    UITabBarController로 잘못입력한 구문 수정.

--- SideBar2/SideBarViewController.swift
+++ SideBar2/SideBarViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class SideBarViewController: UITabBarController{
+class SideBarViewController: UITableViewController{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
