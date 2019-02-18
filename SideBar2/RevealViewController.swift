9ed18c0311d9f2ce574ce6216e5ddc7066f26f75
정기욱 <rldnr56@me.com>
2019-02-18 15:10:23 +0900

    RevealViewController 클래스는 스토리보드의 가장 앞에 위치하여 프론트 뷰와 리어뷰를 컨트롤할 뷰 컨트롤러.

---
+++ SideBar2/RevealViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RevealViewController.swift
+//  SideBar2
+//
+//  Created by 정기욱 on 18/02/2019.
+//  Copyright © 2019 Kiwook. All rights reserved.
+//
+
+import UIKit
+
+class RevealViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
